b0ed834200a2ebbc28c85d68cd0323dfed853214
Cappy_bara <kacper916@gmail.com>
2021-11-09 23:58:16 +0100

    🎨Token location moved from interceptor to header

--- faketory-app/src/API/axiosConfig.ts
+++ faketory-app/src/API/axiosConfig.ts
@@ -4,31 +4,18 @@ var i = 0;
 
 const axiosInstance = axios.create({
     baseURL: "http://localhost:5000/",
+    headers: {"Authorization": "NONE"}
 })
 
-// axiosInstance.interceptors.request.use((config:any) => {
-//     config.headers.Authorization = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9lbWFpbGFkZHJlc3MiOiJrYWNwZXI5MTZAZ21haWwuY29tIiwiZXhwIjoxNjM3MTgyNzE5LCJpc3MiOiJodHRwczovL2Zha2V0b3J5LmNvbSIsImF1ZCI6Imh0dHBzOi8vZmFrZXRvcnkuY29tIn0.JmbBPibzoRfdDofFt0PCcAgjf8_D8K8Ivwm_smQCgV4";
-//     return config;
-// })
-
 const initializeToken = (token: string) => {
-
-    axiosInstance.interceptors.request.use((config: any) => {
-        config.headers.Authorization = `Bearer ${token}`;
-        return config;
-    })
-}
-
-const logout = () => {
-    console.log(axiosInstance.interceptors.request)
-    axiosInstance.interceptors.request.eject(i);
-    i++;
+    if(axiosInstance.defaults.headers){
+        axiosInstance.defaults.headers = {'Authorization': `Bearer ${token}`};
     }
+}
 
 const axiosElement = {
     axiosInstance: axiosInstance,
     initializeToken: initializeToken,
-    logout: logout
 }
 
 export default axiosElement;
--- faketory-app/src/Components/Header/HeaderComponent.tsx
+++ faketory-app/src/Components/Header/HeaderComponent.tsx
@@ -11,12 +11,13 @@ const HeaderComponent = () => {
     const loggedUser = useSelector<IState, String|null>(state => state.loggedUser);
 
     const handleClick = () => {
-        axiosInstance.logout();
         dispatch(setLogout());
     }
 
     return (
         <div className="header">
+            <div className="text title ms-3 float-start">F@KETORY v.1.0</div>
+            <div className="text me mt-2 ms-1 float-start">by Kacper Pacholczak</div>
             <div className="button text float-end mx-4" onClick={handleClick}>Log out</div>
             <div className="text float-end">Logged as: {loggedUser}</div>
         </div>
