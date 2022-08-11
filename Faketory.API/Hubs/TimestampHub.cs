38ffc38645d8ecbbb93ec22e845943a74e58e08a
Kacper Pacholczak <kacper916@gmail.com>
2022-08-11 22:16:06 +0200

    signalR timestamp implemented

---
+++ Faketory.API/Hubs/TimestampHub.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Faketory.API.Dtos.ActionResponses;
+using Faketory.API.Dtos.Pallets.Responses;
+using Faketory.Application.Services.Interfaces;
+using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Faketory.API.Hubs
+{
+    public class TimestampHub : Hub
+    {
+        private readonly TimestampTicker _timestampTicker;
+
+        public TimestampHub(TimestampTicker timestampTicker)
+        {
+            _timestampTicker = timestampTicker;
+        }
+
+        public void StartTimestamping(string userEmail)
+        {
+            _timestampTicker.Start(userEmail);
+        }
+
+        public async Task StopTimestamping()
+        {
+            await _timestampTicker.Stop();
+        }
+    }
+}
--- faketory-app/src/Components/MenuBar/MainTab/MainTabComponent.tsx
+++ faketory-app/src/Components/MenuBar/MainTab/MainTabComponent.tsx
@@ -1,25 +1,37 @@
 import { useEffect, useState } from "react";
 import { Button, ToggleButton } from "react-bootstrap";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { timestamp } from "../../../API/Actions/actions";
 import { setAnimationState } from "../../../States/animationSource/actions";
 import { updateUserConveyors } from "../../../States/devices/userConveyors/actions";
 import { updateUserMachines } from "../../../States/devices/userMachines/actions";
 import { modifyUserPallets } from "../../../States/devices/userPallets/actions";
 import { updateUserSensors } from "../../../States/devices/userSensors/actions";
+import * as signalR from "@microsoft/signalr";
+import { timestampResponse } from "../../../API/Actions/types";
+import { IState } from "../../../States";
+
 
 const MainTabComponent = () => {
 
     const [autoTimestamp, setAutoTimestamp] = useState<boolean>(true);
+    const [socketAutoTimestamp, setSocketAutoTimestamp] = useState<boolean>(true);
+    const userEmail = useSelector<IState, string | null>(state => state.loggedUser);
+    const [connection,setConnection] = useState<signalR.HubConnection|null>(null); 
+
     const dispatch = useDispatch();
+
+    const dispatchTimestampData = (data : timestampResponse) => {
+        data.sensors && dispatch(updateUserSensors(data.sensors));
+        data.pallets && dispatch(modifyUserPallets(data.pallets));
+        data.conveyors && dispatch(updateUserConveyors(data.conveyors));
+        data.machines && dispatch(updateUserMachines(data.machines));
+        dispatch(setAnimationState());
+    }
 
     const handleTimestamp = () => {
         timestamp().then(response => {
-            response.sensors && dispatch(updateUserSensors(response.sensors));
-            response.pallets && dispatch(modifyUserPallets(response.pallets));
-            response.conveyors && dispatch(updateUserConveyors(response.conveyors));
-            response.machines && dispatch(updateUserMachines(response.machines));
-            dispatch(setAnimationState());
+            dispatchTimestampData(response)
         })
     };
 
@@ -29,6 +41,37 @@ const MainTabComponent = () => {
         return () => clearInterval(interval);
     };
     }, [autoTimestamp]);
+
+    const handleSocketTimestamp = (data : timestampResponse) => {
+        console.log("TIMESTAMP")
+        dispatchTimestampData(data);
+    }
+
+    const startOrStopAutoTimestamp = (shouldNotStart : boolean) => {
+        setSocketAutoTimestamp(shouldNotStart);
+        if(shouldNotStart){
+            connection!.invoke("StopTimestamping");
+        }
+        else{
+            connection!.invoke("StartTimestamping",userEmail);
+        }
+    }
+
+    useEffect(() => {
+        if(!connection){
+            let newConnection = new signalR.HubConnectionBuilder()
+            .withUrl('https://localhost:44368/timestamphub', {
+                skipNegotiation: true,
+                transport: signalR.HttpTransportType.WebSockets
+            })
+            .build();
+    
+            newConnection.start();
+    
+            newConnection.on('timestamp', (data : timestampResponse) => handleSocketTimestamp(data));
+            setConnection(newConnection);
+        }
+    },[]);
 
     return (
 
@@ -51,13 +94,30 @@ const MainTabComponent = () => {
                 variant="outline-primary"
                 checked={autoTimestamp}
                 value="0"
-                onChange={(e) =>
-                    setAutoTimestamp(e.currentTarget.checked)
+                onChange={(e) =>{
+                    setAutoTimestamp(e.currentTarget.checked)}
                 }
             >
                 Auto Timestamp
             </ToggleButton>
+
+
+            <ToggleButton
+                className="mb-2"
+                id="toggle-check-socket"
+                type="checkbox"
+                variant="outline-primary"
+                checked={socketAutoTimestamp}
+                value="0"
+                onChange={(e) =>{
+                    startOrStopAutoTimestamp(e.currentTarget.checked)}
+                }
+            >
+                Socket auto timestamp
+            </ToggleButton>
         </div>
+
+
     )
 }
 
